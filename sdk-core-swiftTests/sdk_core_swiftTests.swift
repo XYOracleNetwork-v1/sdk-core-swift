aa7a3a152474d6ef9ab7f715f5cdd96dc503e891
Carter Harrison <carterjharrison@gmail.com>
2019-01-22 13:42:37 -0800

    Initial Commit

---
+++ sdk-core-swiftTests/sdk_core_swiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  sdk_core_swiftTests.swift
+//  sdk-core-swiftTests
+//
+//  Created by Carter Harrison on 1/22/19.
+//  Copyright © 2019 XYO Network. All rights reserved.
+//
+
+import XCTest
+@testable import sdk_core_swift
+
+class sdk_core_swiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
