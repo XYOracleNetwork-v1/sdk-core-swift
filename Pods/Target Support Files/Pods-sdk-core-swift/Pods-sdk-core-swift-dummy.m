eebcf5408b21ef95440e173e6ac31ee3fcedca48
Carter Harrison <carterjharrison@gmail.com>
2019-01-22 17:17:21 -0800

    Revert "project structure and hashing"
    This reverts commit 435dac1548c85a279adbf7fe7cf582f24db584f1.

--- Pods/Target Support Files/Pods-sdk-core-swift/Pods-sdk-core-swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sdk_core_swift : NSObject
-@end
-@implementation PodsDummy_Pods_sdk_core_swift
-@end
--- Pods/Target Support Files/sdk-objectmodel-swift/sdk-objectmodel-swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_sdk_objectmodel_swift : NSObject
-@end
-@implementation PodsDummy_sdk_objectmodel_swift
-@end
