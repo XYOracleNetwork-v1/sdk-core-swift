6ac64bf941ff92730381be235e53607888645cb2
Darren Sutherland <darren@xyo.network>
2019-06-01 14:56:12 -0700

    refactor: create the central file manager

--- sdk-core-swift/network/appGroup/XyoAppGroupPipe.swift
+++ sdk-core-swift/network/appGroup/XyoAppGroupPipe.swift
@@ -14,52 +14,40 @@ public class XyoAppGroupPipe {
 
     typealias SendCompletionHandler = ([UInt8]?) -> ()
 
-    fileprivate let groupIdentifier: String
-
-    fileprivate struct Constants {
-        static let filename = "whatever"
-        static let fileExtension = "xyonetwork"
-    }
-
-    fileprivate let bundleIdentifier: String
-
-    fileprivate let fileCoordinator = NSFileCoordinator()
-    fileprivate let opQueue = OperationQueue()
-
-    fileprivate let fileUrl: URL?
-
-    fileprivate var monitor: FileMonitor?
-
     fileprivate weak var manager: XyoAppGroupManagerListener?
 
     fileprivate var completionHandler: SendCompletionHandler?
 
     fileprivate let initiationData : XyoAdvertisePacket?
 
+    fileprivate let fileManager: XyoSharedFileManager?
+
     public init(groupIdentifier: String, bundleIdentifier: String, manager: XyoAppGroupManagerListener, initiationData : XyoAdvertisePacket? = nil) {
-        // Group ID mus tbe the same on both ends of the pipe
-        self.groupIdentifier = groupIdentifier
-
-        // Used for distinct pipe files
-        self.bundleIdentifier = bundleIdentifier
-
-        // Used for closing the connectiong
-        self.manager = manager
-
         self.initiationData = initiationData
+        self.fileManager = XyoSharedFileManager(for: bundleIdentifier, groupIdentifier: groupIdentifier)
+        self.fileManager?.setReadListenter(self.listenForResponse)
 
-        // Create/open file that is used to transmit the data
-        let baseUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: self.groupIdentifier)
-        self.fileUrl = baseUrl?.appendingPathComponent(self.bundleIdentifier).appendingPathExtension(Constants.fileExtension)
 
-        // Listen for responses from send() below, using the supplised completion handler
-        self.listenForResponse()
+//        // Group ID mus tbe the same on both ends of the pipe
+//        self.groupIdentifier = groupIdentifier
+//
+//        // Used for distinct pipe files
+//        self.bundleIdentifier = bundleIdentifier
+//
+//        // Used for closing the connectiong
+//        self.manager = manager
+//
+//        self.initiationData = initiationData
+//
+//        // Create/open file that is used to transmit the data
+//        let baseUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: self.groupIdentifier)
+//        self.fileUrl = baseUrl?.appendingPathComponent(self.bundleIdentifier).appendingPathExtension(Constants.fileExtension)
+//
+//        // Listen for responses from send() below, using the supplised completion handler
+//        self.listenForResponse()
     }
 
-    deinit {
-        self.fileCoordinator.cancel()
-        self.opQueue.cancelAllOperations()
-    }
+
 
 }
 
@@ -75,31 +63,40 @@ extension XyoAppGroupPipe: XyoNetworkPipe {
     }
 
     public func send(data: [UInt8], waitForResponse: Bool, completion: @escaping ([UInt8]?) -> ()) {
-        guard let url = self.fileUrl else {
-            completion(nil)
-            return
+        self.fileManager?.write(data: data) { error in
+            guard error == nil else {
+                completion(nil)
+                return
+            }
         }
 
-        // Set the response handler
         self.completionHandler = completion
 
-        var error: NSError?
-        self.opQueue.addOperation { [weak self] in
-            guard let strong = self else { return }
-
-            // Create the message with the data and write to the file
-            let message = Message(data: data, bundleIdentifier: strong.bundleIdentifier)
-            strong.fileCoordinator.coordinate(writingItemAt: url, options: .forReplacing, error: &error) { url in
-                let dictData = NSKeyedArchiver.archivedData(withRootObject: message.encoded)
-                try? dictData.write(to: url)
-            }
-
-            if error != nil { completion(nil) }
-        }
+//        guard let url = self.fileUrl else {
+//            completion(nil)
+//            return
+//        }
+//
+//        // Set the response handler
+//        self.completionHandler = completion
+//
+//        var error: NSError?
+//        self.opQueue.addOperation { [weak self] in
+//            guard let strong = self else { return }
+//
+//            // Create the message with the data and write to the file
+//            let message = Message(data: data, bundleIdentifier: strong.bundleIdentifier)
+//            strong.fileCoordinator.coordinate(writingItemAt: url, options: .forReplacing, error: &error) { url in
+//                let dictData = NSKeyedArchiver.archivedData(withRootObject: message.encoded)
+//                try? dictData.write(to: url)
+//            }
+//
+//            if error != nil { completion(nil) }
+//        }
     }
 
     public func close() {
-        self.manager?.onClose(bundleIdentifier: self.bundleIdentifier)
+//        self.manager?.onClose(bundleIdentifier: self.bundleIdentifier)
     }
 
 }
@@ -107,119 +104,28 @@ extension XyoAppGroupPipe: XyoNetworkPipe {
 // MARK: Handles the response from the other side of the pipe
 fileprivate extension XyoAppGroupPipe {
 
-    // Called from init()
-    func listenForResponse() {
-        guard let url = self.fileUrl else { return }
-
-        // Monitor file changes and emit the event on changes
-        self.monitor = FileMonitor(path: url.path)
-        self.monitor?.onFileEvent = { [weak self] in
-            self?.unpackAndRespond(url)
-        }
+    func listenForResponse(_ data: [UInt8]?) {
+        self.completionHandler?(data)
     }
 
-    func unpackAndRespond(_ url: URL) {
-        guard
-            let containerData = NSKeyedUnarchiver.unarchiveObject(withFile: url.path) as? Data,
-            let message = Message.decode(containerData),
-            message.bundleIdentifier != self.bundleIdentifier else { return }
-
-        self.completionHandler?(message.data)
-    }
-
-}
-
-// MARK: Simple serializable container to store the data + sender id
-fileprivate extension XyoAppGroupPipe {
-
-    struct Message: Codable {
-        let data: [UInt8], bundleIdentifier: String
-
-        var encoded: Data? {
-            let encoder = JSONEncoder()
-            let jsonData = try? encoder.encode(self)
-            return jsonData
-        }
-
-        static func decode(_ data: Data) -> Message? {
-            let decoder = JSONDecoder()
-            let decoded = try? decoder.decode(Message.self, from: data)
-            return decoded
-        }
-    }
-
-}
-
-// Watches the file so it can transmit data to other listeners
-internal class FileMonitor {
-
-    private let filePath: String
-    private let fileSystemEvent: DispatchSource.FileSystemEvent
-    private let dispatchQueue: DispatchQueue
-
-    private var eventSource: DispatchSourceFileSystemObject?
-
-    internal var onFileEvent: (() -> ())? {
-        willSet {
-            self.eventSource?.cancel()
-        }
-        didSet {
-            if (onFileEvent != nil) {
-                self.startObservingFileChanges()
-            }
-        }
-    }
-
-    internal init?(path: String,
-                   observeEvent: DispatchSource.FileSystemEvent = .write,
-                   queue: DispatchQueue = DispatchQueue.global(),
-                   createFile create: Bool = true) {
-
-        self.filePath = path
-        self.fileSystemEvent = observeEvent
-        self.dispatchQueue = queue
-
-        if self.fileExists() == false && create == false {
-            return nil
-        } else if self.fileExists() == false {
-            createFile()
-        }
-    }
-
-    deinit {
-        self.eventSource?.cancel()
-    }
-
-    private func fileExists() -> Bool {
-        return FileManager.default.fileExists(atPath: self.filePath)
-    }
-
-    private func createFile() {
-        if self.fileExists() == false {
-            FileManager.default.createFile(atPath: self.filePath, contents: nil, attributes: nil)
-        }
-    }
-
-    private func startObservingFileChanges() {
-        guard self.fileExists() == true else { return }
-
-        let descriptor = open(self.filePath, O_EVTONLY)
-        guard descriptor != -1 else { return }
-
-        self.eventSource = DispatchSource.makeFileSystemObjectSource(
-            fileDescriptor: descriptor,
-            eventMask: self.fileSystemEvent,
-            queue: self.dispatchQueue)
-
-        self.eventSource?.setEventHandler { [weak self] in
-            self?.onFileEvent?()
-        }
-
-        self.eventSource?.setCancelHandler() {
-            close(descriptor)
-        }
-
-        self.eventSource?.resume()
-    }
+//    // Called from init()
+//    func listenForResponse() {
+//        guard let url = self.fileUrl else { return }
+//
+//        // Monitor file changes and emit the event on changes
+//        self.monitor = FileMonitor(path: url.path)
+//        self.monitor?.onFileEvent = { [weak self] in
+//            self?.unpackAndRespond(url)
+//        }
+//    }
+//
+//    func unpackAndRespond(_ url: URL) {
+//        guard
+//            let containerData = NSKeyedUnarchiver.unarchiveObject(withFile: url.path) as? Data,
+//            let message = Message.decode(containerData),
+//            message.bundleIdentifier != self.bundleIdentifier else { return }
+//
+//        self.completionHandler?(message.data)
+//    }
 
 }
--- sdk-core-swift/network/appGroup/XyoAppGroupPipeManager.swift
+++ sdk-core-swift/network/appGroup/XyoAppGroupPipeManager.swift
@@ -19,19 +19,7 @@ public protocol XyoAppGroupManagerListener: class {
 /// Allows for a client to request a pipe be created to connect to a server
 public class XyoAppGroupPipeManager {
 
-    public static let defaultGroupId = "group.network.xyo"
-
     fileprivate lazy var pipes = [String: XyoAppGroupPipe]()
-
-    fileprivate let groupIdentifier: String
-
-    fileprivate let fileCoordinator = NSFileCoordinator()
-    fileprivate let opQueue = OperationQueue()
-
-    fileprivate let fileUrl: URL?
-    fileprivate var monitor: FileMonitor?
-
-    fileprivate let bundleIdentifier: String
 
     fileprivate struct Constants {
         static let filename = "appgrouprequest"
@@ -40,47 +28,60 @@ public class XyoAppGroupPipeManager {
 
     fileprivate let listener: XyoAppGroupPipeListener
 
-    init(listener: XyoAppGroupPipeListener, bundleIdentifier: String, groupIdentifier: String = XyoAppGroupPipeManager.defaultGroupId) {
+    fileprivate let fileManager: XyoSharedFileManager?
+
+    public init(listener: XyoAppGroupPipeListener, bundleIdentifier: String, groupIdentifier: String = XyoSharedFileManager.defaultGroupId) {
         // Notifies on the addition of a new pipe
         self.listener = listener
 
-        // The app's id, used for proper file naming and future whitelisting
-        self.bundleIdentifier = bundleIdentifier
+        self.fileManager = XyoSharedFileManager(
+            for: Constants.filename,
+            groupIdentifier: groupIdentifier)
 
-        // The app group used by both client and server so the request file can be shared
-        self.groupIdentifier = groupIdentifier
-
-        // Create/open file that is used for requesting pipes
-        let baseUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: self.groupIdentifier)
-        self.fileUrl = baseUrl?.appendingPathComponent(Constants.filename).appendingPathExtension(Constants.fileExtension)
-
-        // The "server" listens for requests to connect
-        self.listenForConnectionRequest()
+//        // The app's id, used for proper file naming and future whitelisting
+//        self.bundleIdentifier = bundleIdentifier
+//
+//        // The app group used by both client and server so the request file can be shared
+//        self.groupIdentifier = groupIdentifier
+//
+//        // Create/open file that is used for requesting pipes
+//        let baseUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: self.groupIdentifier)
+//        self.fileUrl = baseUrl?.appendingPathComponent(Constants.filename).appendingPathExtension(Constants.fileExtension)
+//
+//        // The "server" listens for requests to connect
+//        self.listenForConnectionRequest()
     }
 
-    // Called from the client to ask for a pipe to be created for this connection
-    // A nil-error response indicates the
-    func requestConnection(bundleIdentifier: String, initiationData: [UInt8]?, completion: ((Error?) -> Void)? = nil) {
-        guard
-            let url = self.fileUrl,
-            let initiationData = initiationData else {
-                completion?(nil)
-            return
-        }
-
-        var error: NSError?
-        self.opQueue.addOperation { [weak self] in
-            guard let strong = self else { return }
-
-            // Create the message with the data and write to the file
-            let message = Message(bundleIdentifier: bundleIdentifier, initiationData: initiationData)
-            strong.fileCoordinator.coordinate(writingItemAt: url, options: .forReplacing, error: &error) { url in
-                let dictData = NSKeyedArchiver.archivedData(withRootObject: message.encoded)
-                try? dictData.write(to: url)
-            }
-
-            if error != nil { completion?(error) }
-        }
+    // Called from the client to ask for a pipe to be created for this connection, returning a local pipe
+    public func requestConnection(initiationData: [UInt8]?, completion: ((XyoAppGroupPipe?) -> Void)? = nil) {
+//        guard
+//            let url = self.fileUrl,
+//            let initiationData = initiationData else {
+//                completion?(nil)
+//            return
+//        }
+//
+//        var error: NSError?
+//        self.opQueue.addOperation { [weak self] in
+//            guard let strong = self else { return }
+//
+//            // Create the file if it does not exist and write the initiation data
+//            let message = Message(bundleIdentifier: strong.bundleIdentifier, initiationData: initiationData)
+//            strong.fileCoordinator.coordinate(writingItemAt: url, options: .forReplacing, error: &error) { url in
+//                let dictData = NSKeyedArchiver.archivedData(withRootObject: message.encoded)
+//                try? dictData.write(to: url)
+//
+//                // Build the pipe and return it through the completion callback
+//                let pipe = XyoAppGroupPipe(
+//                    groupIdentifier: XyoAppGroupPipeManager.defaultGroupId,
+//                    bundleIdentifier: strong.bundleIdentifier,
+//                    manager: strong)
+//
+//                completion?(pipe)
+//            }
+//
+//            if error != nil { completion?(nil) }
+//        }
     }
 
 }
@@ -94,60 +95,43 @@ extension XyoAppGroupPipeManager: XyoAppGroupManagerListener {
 
 }
 
-// MARK: Simple serializable container to store the data + sender id
-fileprivate extension XyoAppGroupPipeManager {
-
-    struct Message: Codable {
-        let bundleIdentifier: String, initiationData: [UInt8]
-
-        var encoded: Data? {
-            let encoder = JSONEncoder()
-            let jsonData = try? encoder.encode(self)
-            return jsonData
-        }
-
-        static func decode(_ data: Data) -> Message? {
-            let decoder = JSONDecoder()
-            let decoded = try? decoder.decode(Message.self, from: data)
-            return decoded
-        }
-    }
-
-}
-
 // MARK: Server listeners for
 fileprivate extension XyoAppGroupPipeManager {
 
+    func listenForConnectionRequest(_ data: [UInt8]?) {
+
+    }
+
     // Called from init()
-    func listenForConnectionRequest() {
-        guard let url = self.fileUrl else { return }
-
-        // Monitor file changes and create the pipe
-        self.monitor = FileMonitor(path: url.path)
-        self.monitor?.onFileEvent = { [weak self] in
-            self?.registerApp(url)
-        }
-    }
-
-    func registerApp(_ url: URL) {
-        guard
-            let containerData = NSKeyedUnarchiver.unarchiveObject(withFile: url.path) as? Data,
-            let message = Message.decode(containerData),
-            message.bundleIdentifier != self.bundleIdentifier else { return }
-
-        // Create the pipe, setting the initiation data
-        let initiationData = XyoAdvertisePacket(data: message.initiationData)
-        let pipe = self.createPipe(for: message.bundleIdentifier, initiationData: initiationData)
-
-        // Register the pipe
-        self.pipes[message.bundleIdentifier] = pipe
-
-        // Notify listener that the pipe is ready
-        self.listener.onPipe(pipe: pipe)
-    }
-
-    func createPipe(for bundleIdentifier: String, initiationData: XyoAdvertisePacket? = nil) -> XyoAppGroupPipe {
-        return XyoAppGroupPipe(groupIdentifier: self.groupIdentifier, bundleIdentifier: bundleIdentifier, manager: self, initiationData: initiationData)
-    }
+//    func listenForConnectionRequest() {
+//        guard let url = self.fileUrl else { return }
+//
+//        // Monitor file changes and create the pipe
+//        self.monitor = FileMonitor(path: url.path)
+//        self.monitor?.onFileEvent = { [weak self] in
+//            self?.registerApp(url)
+//        }
+//    }
+//
+//    func registerApp(_ url: URL) {
+//        guard
+//            let containerData = NSKeyedUnarchiver.unarchiveObject(withFile: url.path) as? Data,
+//            let message = Message.decode(containerData),
+//            message.bundleIdentifier != self.bundleIdentifier else { return }
+//
+//        // Create the pipe, setting the initiation data
+//        let initiationData = XyoAdvertisePacket(data: message.initiationData)
+//        let pipe = self.createPipe(for: message.bundleIdentifier, initiationData: initiationData)
+//
+//        // Register the pipe
+//        self.pipes[message.bundleIdentifier] = pipe
+//
+//        // Notify listener that the pipe is ready
+//        self.listener.onPipe(pipe: pipe)
+//    }
+//
+//    func createPipe(for bundleIdentifier: String, initiationData: XyoAdvertisePacket? = nil) -> XyoAppGroupPipe {
+//        return XyoAppGroupPipe(groupIdentifier: self.groupIdentifier, bundleIdentifier: bundleIdentifier, manager: self, initiationData: initiationData)
+//    }
 
 }
