ef91b6d88d9d2783caad6708a382c9c71548d584
pllearns <thenewphilliplorenzo@outlook.com>
2019-11-13 11:18:55 -0800

    fix: small rename

--- sdk_core_swiftTests/sdkCoreSwiftTests.swift
+++ sdk_core_swiftTests/sdkCoreSwiftTests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class sdkCoreSwiftTests: XCTestCase {
+class SdkCoreSwiftTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
