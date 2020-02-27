656bc690ec754d0390ce1a5260766ba09dad8492
pllearns <thenewphilliplorenzo@outlook.com>
2020-02-27 13:37:50 -0800

    edit config

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "sdk-core-swift",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13_3)
+        .macOS(.v10_15)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
